026405a485e30ea12f4dc7e442e495c68f5c5143
David Klaper <dklaper@andrew.cmu.edu>
2014-03-20 00:06:33 -0400

    Simple Clustering tools -> Todo integrate clustering into scripts/Folder structure

--- Tools/addClusterToCoNLL.py
+++ Tools/addClusterToCoNLL.py
@@ -4,9 +4,45 @@
 # David Klaper, Carnegie Mellon University
 
 # Script to add the cluster number (or transformations of it) as features to the conll file
-# python addClusterToCoNLLL.py clustering.txt inputCoNLLfiles?
+# python addClusterToCoNLLL.py clustering.txt inputCoNLLfiles...
+
+import sys, codecs
 
 UNK_CLUSTER = "--"
 
+PREFLEN = 4
+
+words = {}
+clusters = set()
+
+def readPaths(fl):
+    with codecs.open(fl, encoding="utf-8") as fin:
+        for line in fin:
+            if line.strip() != "":
+                parts = line.split()
+                words[parts[1]] = parts[0][:PREFLEN]
+                clusters.add(parts[0][:PREFLEN])
+    return words            
+                
 if __name__ == "__main__":
-    pass
+    words = readPaths(sys.argv[1])
+
+    for k in sys.argv[2:]:
+        text = ""
+        with codecs.open(k, encoding="utf-8") as inc:
+            for line in inc:
+                if line.strip() == "":
+                    text += line
+                else:
+                    parts = line.split("\t")
+                    if parts[1] in words:
+                        parts[5] = words[parts[1]]
+                    else:
+                        parts[5] = UNK_CLUSTER
+                        
+                    text += "\t".join(parts)
+                    
+        with codecs.open(k, 'w', encoding="utf-8") as out:
+            out.write(text)
+
+        print len(clusters)
--- Tools/makeClustering.py
+++ Tools/makeClustering.py
@@ -10,5 +10,29 @@
 
 CLUSTERS=800 # Number of clusters
 
+import codecs, subprocess, sys
+
+def makeFile(fllst):
+    with codecs.open("clusters.txt", 'w', encoding="utf-8") as conout:
+        
+      for fn in fllst:
+        text = ""
+        if not fn.endswith("conll"):
+            continue # skip non conllfiles
+        
+        with codecs.open(fn, encoding="utf-8") as conin:
+            for line in conin:
+                if line.strip() == "":
+                    text += " \n"
+                else:
+                    word = line.split("\t")[1]
+                    text += word + " "
+    
+        conout.write(text+" \n")
+            
+            
 if __name__ == "__main__":
-    pass
+    
+    makeFile(sys.argv[1:])
+    
+    
